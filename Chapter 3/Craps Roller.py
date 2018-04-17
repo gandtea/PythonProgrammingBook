0d590bfc943d94d27dce3bcddeed78848cf4e66b
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 19:55:55 +0100

    Create Craps Roller.py

---
+++ Chapter 3/Craps Roller.py
@@ -0,0 +1,14 @@
+# Craps Roller
+# Demonstrates random number generation
+
+import random
+
+# generate random numbers 1 - 6
+die1 = random.randint(1, 6)
+die2 = random.randrange(6) + 1
+
+total = die1 + die2
+
+print("You rolled a", die1, "and a", die2, "for a total of", total)
+
+input("\n\Press the enter key to exit.")
