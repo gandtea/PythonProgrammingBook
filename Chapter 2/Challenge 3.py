6534bfd23d2794b346f11d425b94e9c69b372147
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 19:48:10 +0100

    Create Challenge 3.py

---
+++ Chapter 2/Challenge 3.py
@@ -0,0 +1,7 @@
+# Chapter 2, Challenge 3
+# Tipper Program
+
+total = int(input("How much is your total? "))
+print("Please tip either 15% - £", 0.15*total, " or 20% - £", 0.2*total, sep='')
+
+input("\n\nPress the enter key to exit.")
