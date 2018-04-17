87a10253cc109b573e1320f2ea28a602031ebbf0
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 19:45:34 +0100

    Create Challenge 2.py

---
+++ Chapter 2/Challenge 2.py
@@ -0,0 +1,9 @@
+# Chapter 2, Challenge 2
+# User enters two different types of food and the program mashes them together.
+
+fav_food1 = input("What is your first favourite food? ")
+fav_food2 = input("What is your second favourite food? ")
+
+print("\nFave food mashup = ", fav_food1, fav_food2, sep='')
+
+input("\n\nPress the enter key to exit.")
