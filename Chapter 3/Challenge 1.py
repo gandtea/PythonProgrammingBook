285c5aa5d8e1b4b1312963ef9c434eb2d5305b11
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 21:03:55 +0100

    Create Challenge 1.py

---
+++ Chapter 3/Challenge 1.py
@@ -0,0 +1,20 @@
+# Flip A Coin program
+
+import random
+
+count = 0
+heads = 0
+tails = 0
+
+while count != 100:
+    count += 1
+    flip = random.randint(1,2)
+    if flip == 1:
+        heads += 1
+    else:
+        tails += 1
+
+print("Total heads flipped: ", heads)
+print("Total tails flipped: ", tails)
+
+input("\n\nPress the enter key to exit.")
