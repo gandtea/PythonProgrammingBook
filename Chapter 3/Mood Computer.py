450db99048cb7761bc803203bf5b221f02f3918f
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 20:16:23 +0100

    Create Mood Computer.py

---
+++ Chapter 3/Mood Computer.py
@@ -0,0 +1,59 @@
+# Mood Computer
+# Demonstrates the elif clause
+
+import random
+
+print("I sense your energy. You true emotions are coming across my screen.")
+print("You are...")
+
+mood = random.randint(1, 3)
+
+if mood = 1:
+    # happy
+    print( \
+    """
+
+        -----------
+       |          |
+       |  O    O  |
+       |    <     |
+       |          |
+       |  .     . |
+       |   '...'  |
+        -----------
+                   """)
+elif mood = 2:
+    # neutral
+    print( \
+    """
+
+        -----------
+       |          |
+       |  O    O  |
+       |    <     |
+       |          |
+       |  ------  |
+       |          |
+        -----------
+                   """)
+
+elif mood = 3:
+    # sad
+    print( \
+    """
+
+        -----------
+       |          |
+       |  O    O  |
+       |    <     |
+       |          |
+       |   .'.    |
+       |  '   '   |
+        -----------
+                   """)
+else:
+    print("Illegal mood value! (You must be in a really bad mood).")
+
+print("...today.")
+
+input("\n\nPress the enter key to exit.")
