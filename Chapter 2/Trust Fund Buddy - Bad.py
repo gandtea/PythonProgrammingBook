237a62c741758e8d78350384e4e9bcb68e86e037
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 18:54:19 +0100

    Create Trust Fund Buddy - Bad.py

---
+++ Chapter 2/Trust Fund Buddy - Bad.py
@@ -0,0 +1,30 @@
+# Trust Fund Buddy - Bad
+# Demonstrates a logical error
+
+print(
+"""
+            Trust Fund Buddy
+
+Totals your monthly spending so that you trust fund doesn't run out
+(and you're forced ti get a real job).
+
+Please enter the requested, monthly costs. Since you're rich, ignore
+pennies and use only dollar amounts.
+
+"""
+)
+
+car = input("Lamborghini Tune-Ups: ")
+rent = input("Manhattan Apartment: ")
+jet = input("Private Jet Rental: ")
+gifts = input("Gifts: ")
+food = input("Dining Out: ")
+staff = input("Staff (butlers, chef, driver, assistant): ")
+guru = input("Personal Guru and Coach: ")
+games = input("Computer Games: ")
+
+total = car + rent + jet + gifts + food + staff + guru + games
+
+print("\nGrant Total:", total)
+
+input("\n\nPress the enter key to exit.")
