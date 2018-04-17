90a39c893c1e12011b589f3364026375fa6cd48c
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 20:02:56 +0100

    Create Password.py

---
+++ Chapter 3/Password.py
@@ -0,0 +1,12 @@
+# Password
+# Demonstrates the if statement
+
+print("Welcome to System Securtiy Inc.")
+print("-- where security is our middle name\n")
+
+password = input("Enter your password: ")
+
+if password == "secret":
+    print("Access Granted")
+
+input("\n\nPress the enter key to exit.")
