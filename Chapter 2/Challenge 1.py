45baabce800f4ab6261011a11d306e8d29230aa5
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 19:39:36 +0100

    Create Challenge 1.py

---
+++ Chapter 2/Challenge 1.py
@@ -0,0 +1,24 @@
+# Chapter 2, Challenge 1
+# Legal Variable names
+
+hello = "hello"
+Hello = "Hello"
+MyNameIs = "MyNameIs is OK - but it's better to start with a lower case"
+my_name_is = "my_name_is"
+CAPITAL_LETTERS = "Constant variables should be in capital letters."
+
+
+# Illegal Variable names
+
+# 12345 - starts with a number
+# a1234 das - no spaces allowed
+# @£$! - variable names can only contain numbers, letters and underscores
+
+
+# Good and bad
+
+# Also recommended to keep the short, but descriptive.
+# Don't start the variable with an underscore - they have a special
+# meaning in Python.
+# Be consistent!
+# Be Pythonic!
