b696c536e8b9681709b187fa46a56d86a6e48fbd
Georgina Dean <georgy.dean@gmail.com>
2018-04-17 19:51:20 +0100

    Update Challenge 3.py

--- Chapter 2/Challenge 3.py
+++ Chapter 2/Challenge 3.py
@@ -2,6 +2,7 @@
 # Tipper Program
 
 total = int(input("How much is your total? "))
-print("Please tip either 15% - £", 0.15*total, " or 20% - £", 0.2*total, sep='')
+print("Please tip either 15% - £", 0.15 * total, \
+      " or 20% - £", 0.2 * total, sep='')
 
 input("\n\nPress the enter key to exit.")
