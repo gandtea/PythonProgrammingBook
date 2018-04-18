55ebef00117a18e2c6a2ff8e12dbd011ad9c0751
Georgina Dean <georgy.dean@gmail.com>
2018-04-18 10:28:37 +0100

    Update Challenge 4.py

--- Chapter 2/Challenge 4.py
+++ Chapter 2/Challenge 4.py
@@ -1,5 +1,9 @@
 # Chapter 2, Challenge 4
-# Car Salesman program
+# Write a Car Salesman program where the user enters the base price of a car. 
+# The program should add on a bunch of extra fees such as tax, license, dealer prep, and destination charge. 
+# Make tax and license a percent of the base price. 
+# The other fees should be set values. 
+# Display the actual price of the car once all the extras are applied.
 
 base_price = int(input("What is the base price of the car? "))
 
