d4dd3472859df7a29d08f09b7e288e3b7e1c5672
Georgina Dean <georgy.dean@gmail.com>
2018-04-18 10:12:23 +0100

    Create No Vowels.py

---
+++ Chapter 4/No Vowels.py
@@ -0,0 +1,16 @@
+# No Vowels
+# Demonstrates creating new strings with a for loop
+
+message = input("Enter a message: ")
+new_message = ""
+VOWELS = "aeiou"
+
+print()
+for letter in message:
+    if letter.lower() not in VOWELS:
+        new_message += letter
+        print("A new string has been created:", new_message)
+
+print("\nYour message without vowels is:", new_message)
+
+input("\n\nPress the enter key to exit.")
