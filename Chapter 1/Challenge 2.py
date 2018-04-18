4f8ea1dc7841ee3a6e9cca20fc8f5588f594e02f
Georgina Dean <georgy.dean@gmail.com>
2018-04-18 10:21:02 +0100

    Update Challenge 2.py

--- Chapter 1/Challenge 2.py
+++ Chapter 1/Challenge 2.py
@@ -1,5 +1,6 @@
-# Print My Name
 # Chapter 1, Challenge 2
+# Write and save a program that prints out your name and waits for the user to press the 
+# Enter key before the program ends. Then, run the program by double-clicking its icon.
 
 print('Georgy')
 input('Press enter to exit program.')
