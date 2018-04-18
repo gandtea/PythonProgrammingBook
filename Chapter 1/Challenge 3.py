cc804d81277a83b5b559c6c7bc712f5b9f63933c
Georgina Dean <georgy.dean@gmail.com>
2018-04-18 10:21:57 +0100

    Update Challenge 3.py

--- Chapter 1/Challenge 3.py
+++ Chapter 1/Challenge 3.py
@@ -1,5 +1,8 @@
-# Print Quote program
 # Chapter 1, Challenge 3
+# Write a program that prints your favourite quote. 
+# It should give credit to the person who said it on the next line.
 
 print("He's not the messiah, he's a very naughty boy.")
 print("Brian's mum in Monty Python's Life of Brian.")
+
+input("Press enter to exit.")
