7467f268042dbf7d2a4583de6e27d24c8fcc26ef
Georgina Dean <georgy.dean@gmail.com>
2018-04-22 21:34:28 +0100

    Create Simple Game.py

---
+++ Chapter 9/Simple Game.py
@@ -0,0 +1,26 @@
+# Simple Game
+# Demonstrates importing modules
+# Requires the games module to be in the same directory as this game
+
+import games, random
+
+print("Welcome to the world's simplest game!\n")
+
+again = None
+while again != "n":
+    players = []
+    num = games.ask_number(question = "How many players?? (2-5): ", low = 2, \
+                           high = 5)
+    for i in range(num):
+        name = input("Player name: ")
+        score = random.randrange(100) + 1
+        player = games.Player(name, score)
+        players.append(player)
+
+    print("\Here are the game results:")
+    for player in players:
+        print(player)
+
+    again = games.ask_yes_no("\nDo you want to play again? (y/n): ")
+
+input("\n\nPress the enter key to exit.")
