fb2475c004c54270a96dab7459ff1f91cdc0083b
Georgina Dean <georgy.dean@gmail.com>
2018-04-22 21:32:47 +0100

    Create Games module.py

---
+++ Chapter 9/Games module.py
@@ -0,0 +1,31 @@
+# Games
+# Demonstrates module creation
+
+class Player(object):
+    """A Player for a game."""
+    def __init__(self, name, score = 0):
+        self.name = name
+        self.score = score
+
+    def __str__(self):
+        rep = self.name + ":\t" + str(self.score)
+        return rep
+
+def ask_yes_no(question):
+    """Ask a yes or no question."""
+    response = None
+    while response not in ("y", "n"):
+        response = input(question).lower()
+    return response
+
+def ask_number(question, low, high):
+    """Ask for a number within a range."""
+    response = None
+    while response not in range(low, high):
+        response = int(input(question).lower())
+    return response
+
+if __name__ == "__main__":
+    print("You ran this module directly (and did not 'import' it).")
+    input("Press enter key to exit.")
+
