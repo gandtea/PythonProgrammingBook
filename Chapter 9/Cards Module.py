8cdd6fda37cf674b6afb9eee488114abe23054aa
Georgina Dean <georgy.dean@gmail.com>
2018-04-23 10:14:49 +0100

    Update Cards Module.py

--- Chapter 9/Cards Module.py
+++ Chapter 9/Cards Module.py
@@ -3,9 +3,9 @@
 
 class Card(object):
     """A playing card."""
-    RANKS = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", \
-             "J", "Q", "K")
-    SUITS = ("c", "d", "h", "s")
+    RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", \
+             "J", "Q", "K"]
+    SUITS = ["c", "d", "h", "s"]
 
     def __init__(self, rank, suit, face_up = True):
         self.rank = rank
